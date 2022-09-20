010802f7605013d323869bf814cc0ddc4c4bed57
PhucVu <phucvu@futurify.vn>
2022-09-21 00:37:03 +0700

    Learning describe

--- src/Components/Greet/Greet.test.tsx
+++ src/Components/Greet/Greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import Greet from "./Greet";
+import Greet from "./greet";
 
-it("should render hello to the screen", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  it("should render hello to the screen", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-it.only("should render name is Tien Phuc", () => {
-  render(<Greet name="Tien Phuc" />);
-  const textElement = screen.getByText(/Tien Phuc/i);
-  expect(textElement).toBeInTheDocument();
+describe("Another Greet", () => {
+  it("should render name is Tien Phuc", () => {
+    render(<Greet name="Tien Phuc" />);
+    const textElement = screen.getByText(/Tien Phuc/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
